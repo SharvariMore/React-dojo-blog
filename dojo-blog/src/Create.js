e30e4ec43803615502ada54c4d2f64da753abb48
Sharvari More <83008601+SharvariMore@users.noreply.github.com>
2021-12-31 16:01:23 +0530

    Added form details

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import { useHistory } from "react-router-dom";
+
 const Create = () => {
+    const [title,setTitle] = useState('');
+    const [body,setBody] = useState('');
+    const [author,setAuthor] = useState('Sania');
+    const history = useHistory('');
+
+    const handleSubmit= (e) =>
+    {
+        e.preventDefault();
+        const blog = {title, body, author};
+        console.log(blog);
+        history.push('/');
+    }
     return (
       <div className="create">
         <h2>Add a New Blog</h2>
+        <form onSubmit={handleSubmit}>
+            <label>Blog Title: </label>
+            <input type="text" required value={title} onChange={(e) => setTitle(e.target.value)}></input>
+            <label>Blog Body: </label>
+            <textarea required value={body} onChange={(e) => setBody(e.target.value)}></textarea>
+            <label>Blog Author: </label>
+            <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+                <option value="Sania">Sania</option>
+                <option value="Deepika">Deepika</option>
+            </select>
+            <button>Add Blog</button>
+            <p>{title}</p>
+            <p>{body}</p>
+            <p>{author}</p>
+        </form>
       </div>
     );
   }
